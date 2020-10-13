574fc619157ef8f50c8b41cdc20a828533109f3e
Tar0r <mr.tator72@gmail.com>
2020-10-13 22:45:08 +0300

    Nothing but hello world

---
+++ 215Labs2020/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _215Labs2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //StudentBaseGetSet[]name = new Sabirov.StudentBaseGetSet[5];
+            //StudentBaseGetSet[] marks = new Sabirov.StudentBaseGetSet[3];
+            // Console.Write("Введите Фамилию Имя ученика: ");
+            // for (int i = 0; i < name.Length; i++)
+            // {
+            // //name[i].Name = Console.ReadLine();
+            // Console.Write("Введите оценку по математике: ");
+            // marks[0].Marks = int.Parse(Console.ReadLine());
+            // Console.Write("Введите оценку по русскому языку: ");
+            // marks[1].Marks = int.Parse(Console.ReadLine());
+            // Console.Write("Введите оценку по английскому: ");
+            // marks[2].Marks = int.Parse(Console.ReadLine());
+            Console.WriteLine("Идут два моряка по набережной и тут один говорит: А я и не моряк...");
+
+            }
+            
+            
+        }
+    
+}
---
+++ 215Labs2020/Sabirov/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020.Sabirov
+{
+    class StudentBaseGetSet
+    {
+        private string name;
+        private int marks;
+
+        public int Marks
+        {
+            get
+            {
+                return marks;
+            }
+            set
+            {
+                if (value < 2) marks = 2;
+                else if (value > 5) marks = 5;
+                else marks = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
