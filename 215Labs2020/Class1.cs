edb144be0177a479fe4be9de9008ea2cb8249172
shkk03 <sh_k_i23@bk.ru>
2020-10-13 23:39:06 +0300

    hello

---
+++ 215Labs2020/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _215Labs2020
+{
+    class Class1
+    {
+     
+        public void Get()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
